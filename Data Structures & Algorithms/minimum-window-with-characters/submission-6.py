4b081cbdb46b2f0af7d45ee8d6cc5da633333a7f
alreadyforgot <62143514+yrlf@users.noreply.github.com>
2026-07-08 23:06:24 +1000

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        l, r = 0, 0
+        cnt = Counter(t)
+        cnt_s = Counter(s)
+
+        for char, freq in cnt.items():
+            if char not in s or freq > cnt_s[char]:
+                return ""
+
+
+        window = {}
+        valid = 0
+        
+        res = s
+
+        while r < n:
+            c = s[r]
+            window[c] = window.get(c, 0) + 1
+            r += 1
+
+            if c in cnt and window[c] == cnt[c]:
+                valid += 1
+            
+            
+            while valid == len(cnt) and l <= r:
+                if r - l < len(res):
+                    res = s[l:r]
+                d = s[l]
+                if window[d] == cnt[d]:
+                    valid -= 1
+                window[d] -= 1
+                l += 1
+                        
+
+        return res
